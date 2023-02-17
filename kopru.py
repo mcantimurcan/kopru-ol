0a83ea91157b83cc37da333f4e086b03d141f501
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-17 22:24:03 +0300

    basit eşleşme yapıldı ama çıkartılması gerekenler var

--- kopru.py
+++ kopru.py
@@ -6,6 +6,7 @@ db = xl.readxl(fn='datasheet.xlsx')
 gonder_row = db.ws(ws="Kitap Gönder").maxrow
 gonder_col = db.ws(ws="Kitap Gönder").maxcol
 al_row = db.ws(ws="Kitap Al").maxrow
+al_col = db.ws(ws="Kitap Al").maxcol
 
 result = []
 gonder_liste = []
@@ -19,18 +20,19 @@ for i in range(2, gonder_row + 1):
     
 for i in range(2, al_row + 1):
     new = []
-    for j in range(1, gonder_col + 1):
+    for j in range(1, al_col + 1):
         new.append(db.ws(ws="Kitap Al").index(row=i, col=j))
     alici_liste.append(new)
 
+gonder_liste_copy = gonder_liste[:]
+alici_liste_copy = alici_liste[:]
+
 for i in gonder_liste:
-    gonderen_il = i[6]
+    gonderen_il = i[5]
     for j in alici_liste:
-        alici_il = j[5]
+        alici_il = j[6]
         if alici_il == gonderen_il:
             result.append([i[2], j[2]])
-            alici_liste.remove(j)
-            gonder_liste.remove(i)
-    
-    
+            
+            
 print(result)
