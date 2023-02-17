7243085c553dd2a831679e8fa5a23f282fd15607
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-17 22:13:34 +0300

    eşleştirmelere devam ediliyor

--- kopru.py
+++ kopru.py
@@ -4,6 +4,7 @@ import pylightxl as xl
 db = xl.readxl(fn='datasheet.xlsx')
 
 gonder_row = db.ws(ws="Kitap Gönder").maxrow
+gonder_col = db.ws(ws="Kitap Gönder").maxcol
 al_row = db.ws(ws="Kitap Al").maxrow
 
 result = []
@@ -11,17 +12,25 @@ gonder_liste = []
 alici_liste = []
 
 for i in range(2, gonder_row + 1):
-    gonder_liste.append(db.ws(ws="Kitap Gönder").index(row=i, col=3))
+    new = []
+    for j in range(1, gonder_col + 1):
+        new.append(db.ws(ws="Kitap Gönder").index(row=i, col=j))
+    gonder_liste.append(new)
     
 for i in range(2, al_row + 1):
-    alici_liste.append(db.ws(ws="Kitap Al").index(row=i, col=3))
+    new = []
+    for j in range(1, gonder_col + 1):
+        new.append(db.ws(ws="Kitap Al").index(row=i, col=j))
+    alici_liste.append(new)
 
-for i in range(2, al_row + 1):
-    alici_il = db.ws(ws="Kitap Al").index(row=i, col=7)
-    for j in range(2, gonder_row + 1):
-        gonderen_il = db.ws(ws="Kitap Gönder").index(row=j, col=6)
+for i in gonder_liste:
+    gonderen_il = i[6]
+    for j in alici_liste:
+        alici_il = j[5]
         if alici_il == gonderen_il:
-            result.append([db.ws(ws="Kitap Al").index(row=i, col=3), db.ws(ws="Kitap Gönder").index(row=j, col=3)])
+            result.append([i[2], j[2]])
+            alici_liste.remove(j)
+            gonder_liste.remove(i)
     
     
 print(result)
