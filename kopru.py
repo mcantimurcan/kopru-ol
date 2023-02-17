f30f2a92dda0af00928c0411a0f00af137ba1e43
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-17 22:31:54 +0300

    minor changes

--- kopru.py
+++ kopru.py
@@ -12,17 +12,24 @@ result = []
 gonder_liste = []
 alici_liste = []
 
+#gönderen listesi oluşturma
 for i in range(2, gonder_row + 1):
     new = []
     for j in range(1, gonder_col + 1):
         new.append(db.ws(ws="Kitap Gönder").index(row=i, col=j))
     gonder_liste.append(new)
     
+#alıcı listesi oluşturma   
 for i in range(2, al_row + 1):
     new = []
     for j in range(1, al_col + 1):
         new.append(db.ws(ws="Kitap Al").index(row=i, col=j))
     alici_liste.append(new)
+
+#teyitsiz olanları silme
+for i in alici_liste:
+    if i[::-2] == "Doğru Değil":
+        alici_liste.remove(i)
 
 gonder_liste_copy = gonder_liste[:]
 alici_liste_copy = alici_liste[:]
