09580fb27bfbcf41d09aa15849b690bf9c796d16
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-17 21:25:01 +0300

    basit bir eşleştirme yapıldı, fazlalıklar çıkartılıcak.

--- kopru.py
+++ kopru.py
@@ -3,11 +3,25 @@ import pylightxl as xl
 
 db = xl.readxl(fn='datasheet.xlsx')
 
-eslesmis_row = db.ws(ws="Eşleşmiş Liste").maxrow
+gonder_row = db.ws(ws="Kitap Gönder").maxrow
+al_row = db.ws(ws="Kitap Al").maxrow
 
 result = []
+gonder_liste = []
+alici_liste = []
 
-for i in range(1, eslesmis_row + 1):
-    result.append(db.ws(ws="Eşleşmiş Liste").index(row=i, col=1))
+for i in range(2, gonder_row + 1):
+    gonder_liste.append(db.ws(ws="Kitap Gönder").index(row=i, col=3))
+    
+for i in range(2, al_row + 1):
+    alici_liste.append(db.ws(ws="Kitap Al").index(row=i, col=3))
+
+for i in range(2, al_row + 1):
+    alici_il = db.ws(ws="Kitap Al").index(row=i, col=7)
+    for j in range(2, gonder_row + 1):
+        gonderen_il = db.ws(ws="Kitap Gönder").index(row=j, col=6)
+        if alici_il == gonderen_il:
+            result.append([db.ws(ws="Kitap Al").index(row=i, col=3), db.ws(ws="Kitap Gönder").index(row=j, col=3)])
+    
     
 print(result)
